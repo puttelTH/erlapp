fe5b5d560338c5d32d4308cdfb4c22c1cf02f080
psaingtong <psaingtong@gmail.com>
2019-09-10 19:27:08 +0700

    init

---
+++ src/erlapp_sup.erl
@@ -0,0 +1,23 @@
+%%%-------------------------------------------------------------------
+%% @doc erlapp top level supervisor.
+%% @end
+%%%-------------------------------------------------------------------
+-module(erlapp_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/0]).
+
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+start_link() ->
+  supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+init([]) ->
+  SupFlags = #{strategy => one_for_one,
+    intensity => 1,
+    period => 5},
+  ChildSpecs = [],
+  {ok, {SupFlags, ChildSpecs}}.
